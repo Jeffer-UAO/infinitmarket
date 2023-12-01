e855ac27be831cb8fcc3a32cd7dfccd99671e8b4
JefferDevop <somadesarrollo@gmail.com>
2023-12-01 15:31:42 -0500

    Cambio tenant gabriel a infinitymarket

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
-export const BASE_API = "https://gabirel.suprainnovations.store"
+export const BASE_API = "https://infinitymarket.suprainnovations.store"
 export const SEARCH_HISTORY = "search-history";
 export const CART = "cart";
 export const BASE_NAME = "https://res.cloudinary.com/ciberneto/"
