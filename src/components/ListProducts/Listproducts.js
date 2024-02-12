4a32809ce6fb3b30ebca83c822c6d28fafe65850
JefferDevop <somadesarrollo@gmail.com>
2024-02-12 13:02:07 -0500

    Control de cantidad por inventario

--- src/components/ListProducts/Listproducts.js
+++ src/components/ListProducts/Listproducts.js
@@ -2,6 +2,7 @@ import { map } from "lodash";
 import styles from "./ListProduts.module.scss";
 import { Available } from "./Available";
 import { SoldOut } from "./SoldOut";
+import { Stokend } from "./Stokend";
 
 export function Listproducts(props) {
   const { products, title } = props;
@@ -10,16 +11,20 @@ export function Listproducts(props) {
     <div className={styles.listProduct}>
       <h4>{title}</h4>
       <div className={styles.product}>
-        {map(
-          products,
-          (product, index) =>
-            product.productData.active &&
-            (!product.productData.soldout ? (
-              <Available key={index} product={product} />
-            ) : (
-              <SoldOut kay={index} product={product} />
-            ))
-        )}
+        {map(products, (product, index) => (
+          <div key={index}>
+            {product.productData.active &&
+              (!product.productData.soldout ? (
+                product.itemactitemData.qty_available > 0 ? (
+                  <Available key={index} product={product} />
+                ) : (
+                  <Stokend key={index} product={product} />
+                )
+              ) : (
+                <SoldOut kay={index} product={product} />
+              ))}
+          </div>
+        ))}
       </div>
     </div>
   );
